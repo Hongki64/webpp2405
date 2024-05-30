e754ebe9f59369b0bedf326ea13a32f50ac09363
Hongki64 <hgchoi64@gmail.com>
2024-05-30 15:49:48 +0900

    webProgram.py

--- webProgram.py
+++ webProgram.py
@@ -1,28 +1,21 @@
-mport streamlit as st
-from langchain.llms import OpenAI
+ 설치 필요
+# pip install langchain
+import streamlit as st
+from langchain_community.llms import OpenAI
 
 st.title('🍎🍐🍊 나의 AI Chat 🥝🍅🍆')
 
-# Sidebar for OpenAI API Key input
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-# Function to generate response from OpenAI
 def generate_response(input_text):
-    if not openai_api_key.startswith('sk-'):
-        st.warning('유효한 OpenAI API 키를 입력해 주세요!', icon='⚠')
-        return
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
-    try:
-        llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-        response = llm(input_text)
-        st.info(response)
-    except Exception as e:
-        st.error(f"오류가 발생했습니다: {e}")
-
-# Form for user input
 with st.form('my_form'):
-    text = st.text_area('텍스트를 입력하세요:', '무엇을 도와드릴까요?')
-    submitted = st.form_submit_button('제출')
-
-    if submitted:
-        generate_response(text)
+  text = st.text_area('Enter text:', '무엇을 도와드릴까요?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('OpenAI API 인증키를 입력해 주세요!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    
+    generate_response(text)
